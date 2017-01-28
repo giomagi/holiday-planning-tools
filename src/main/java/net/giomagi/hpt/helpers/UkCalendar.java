0989d95f1f664722c312ab810303c0b41b4853cb
giomagi <giomagi@gmail.com>
2017-01-28 14:46:31 +0000

    calendar understands bank holidays (not easter of course)

--- src/main/java/net/giomagi/hpt/helpers/UkCalendar.java
+++ src/main/java/net/giomagi/hpt/helpers/UkCalendar.java
@@ -1,12 +1,25 @@
 package net.giomagi.hpt.helpers;
 
+import com.google.common.collect.ImmutableSet;
+
 import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.Month;
 
-import static java.time.DayOfWeek.SATURDAY;
-import static java.time.DayOfWeek.SUNDAY;
+import static java.time.DayOfWeek.*;
 
 public class UkCalendar {
+
+    private static final ImmutableSet<LocalDate> FUTURE_GOOD_FRIDAYS = ImmutableSet.of(LocalDate.of(2017, 4, 14),
+                                                                                       LocalDate.of(2018, 3, 30),
+                                                                                       LocalDate.of(2019, 4, 19),
+                                                                                       LocalDate.of(2020, 4, 10));
+
+    private static final ImmutableSet<LocalDate> FUTURE_EASTER_MONDAYS = ImmutableSet.of(LocalDate.of(2017, 4, 17),
+                                                                                         LocalDate.of(2018, 4, 2),
+                                                                                         LocalDate.of(2019, 4, 22),
+                                                                                         LocalDate.of(2020, 4, 13));
+
     public static LocalDate addWorkingDays(LocalDate date, Integer workDaysCount) {
 
         for (int i = 0; i < workDaysCount; i++) {
@@ -38,8 +51,53 @@ public class UkCalendar {
         return c;
     }
 
-    private static boolean isABankHoliday(LocalDate date) {
-        // TODO: ...
-        return date.equals(LocalDate.of(2017, 5, 29));
+    // TODO: rename to EnglandCalendarYearXtoY + make it non-static
+    public static boolean isABankHoliday(LocalDate date) {
+        return isNewYearsDay(date)
+                || isGoodFriday(date)
+                || isEasterMonday(date)
+                || isMayDay(date)
+                || isSpringBankHoliday(date)
+                || isLateSummerBankHoliday(date)
+                || isChristmas(date)
+                || isBoxingDay(date);
+    }
+
+    private static boolean isNewYearsDay(LocalDate date) {
+        return date.getMonth() == Month.JANUARY && (
+                (date.getDayOfMonth() == 1 && date.getDayOfWeek() != SATURDAY && date.getDayOfWeek() != SUNDAY)
+                        || ((date.getDayOfMonth() == 2 || date.getDayOfMonth() == 3) && date.getDayOfWeek() == MONDAY));
+    }
+
+    private static boolean isGoodFriday(LocalDate date) {
+        return FUTURE_GOOD_FRIDAYS.contains(date);
+    }
+
+    private static boolean isEasterMonday(LocalDate date) {
+        return FUTURE_EASTER_MONDAYS.contains(date);
+    }
+
+    private static boolean isMayDay(LocalDate date) {
+        return date.getMonth() == Month.MAY && date.getDayOfWeek() == MONDAY && date.getDayOfMonth() < 8;
+    }
+
+    private static boolean isSpringBankHoliday(LocalDate date) {
+        return date.getMonth() == Month.MAY && date.getDayOfWeek() == MONDAY && date.getDayOfMonth() > 24;
+    }
+
+    private static boolean isLateSummerBankHoliday(LocalDate date) {
+        return date.getMonth() == Month.AUGUST && date.getDayOfWeek() == MONDAY && date.getDayOfMonth() > 24;
+    }
+
+    private static boolean isChristmas(LocalDate date) {
+        return date.getMonth() == Month.DECEMBER && (
+                (date.getDayOfMonth() == 25 && date.getDayOfWeek() != SATURDAY && date.getDayOfWeek() != SUNDAY)
+                || (date.getDayOfMonth() == 27 && (date.getDayOfWeek() == MONDAY || date.getDayOfWeek() == TUESDAY)));
+    }
+
+    private static boolean isBoxingDay(LocalDate date) {
+        return date.getMonth() == Month.DECEMBER && (
+                (date.getDayOfMonth() == 26 && date.getDayOfWeek() != SATURDAY && date.getDayOfWeek() != SUNDAY)
+                || (date.getDayOfMonth() == 28 && (date.getDayOfWeek() == MONDAY || date.getDayOfWeek() == TUESDAY)));
     }
 }
--- src/test/java/net/giomagi/hpt/helpers/UkCalendarTest.java
+++ src/test/java/net/giomagi/hpt/helpers/UkCalendarTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import java.time.LocalDate;
 
 import static net.giomagi.hpt.helpers.UkCalendar.addWorkingDays;
+import static net.giomagi.hpt.helpers.UkCalendar.isABankHoliday;
 import static net.giomagi.hpt.helpers.UkCalendar.workDaysBetween;
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.*;
@@ -36,5 +37,64 @@ public class UkCalendarTest {
     public void workDaysCalculationRequiresOrderedDates() {
 
         workDaysBetween(LocalDate.now().plusDays(1), LocalDate.now());
+    }
+
+    @Test
+    public void understandsBankHolidays() {
+
+        // new years day
+        assertThat(isABankHoliday(LocalDate.of(2016, 1, 1)), equalTo(true));
+        assertThat(isABankHoliday(LocalDate.of(2016, 1, 2)), equalTo(false));
+        assertThat(isABankHoliday(LocalDate.of(2016, 1, 3)), equalTo(false));
+
+        assertThat(isABankHoliday(LocalDate.of(2017, 1, 1)), equalTo(false));
+        assertThat(isABankHoliday(LocalDate.of(2017, 1, 2)), equalTo(true));
+        assertThat(isABankHoliday(LocalDate.of(2017, 1, 3)), equalTo(false));
+
+        assertThat(isABankHoliday(LocalDate.of(2022, 1, 1)), equalTo(false));
+        assertThat(isABankHoliday(LocalDate.of(2022, 1, 2)), equalTo(false));
+        assertThat(isABankHoliday(LocalDate.of(2022, 1, 3)), equalTo(true));
+
+        // not testing good friday and easter monday, i will just hold a hardcoded list
+
+        // may day bank holiday
+        assertThat(isABankHoliday(LocalDate.of(2017, 5, 1)), equalTo(true));
+        assertThat(isABankHoliday(LocalDate.of(2017, 5, 8)), equalTo(false));
+
+        assertThat(isABankHoliday(LocalDate.of(2018, 5, 1)), equalTo(false));
+        assertThat(isABankHoliday(LocalDate.of(2018, 5, 7)), equalTo(true));
+
+        // spring bank holiday
+        assertThat(isABankHoliday(LocalDate.of(2017, 5, 29)), equalTo(true));
+        assertThat(isABankHoliday(LocalDate.of(2018, 5, 28)), equalTo(true));
+        assertThat(isABankHoliday(LocalDate.of(2020, 5, 25)), equalTo(true));
+        assertThat(isABankHoliday(LocalDate.of(2021, 5, 31)), equalTo(true));
+
+        // late summer bank holiday
+        assertThat(isABankHoliday(LocalDate.of(2017, 8, 28)), equalTo(true));
+        assertThat(isABankHoliday(LocalDate.of(2018, 8, 27)), equalTo(true));
+        assertThat(isABankHoliday(LocalDate.of(2020, 8, 31)), equalTo(true));
+        assertThat(isABankHoliday(LocalDate.of(2025, 8, 25)), equalTo(true));
+
+        // christmas + boxing day
+        assertThat(isABankHoliday(LocalDate.of(2014, 12, 25)), equalTo(true));
+        assertThat(isABankHoliday(LocalDate.of(2014, 12, 26)), equalTo(true));
+        assertThat(isABankHoliday(LocalDate.of(2014, 12, 27)), equalTo(false));
+        assertThat(isABankHoliday(LocalDate.of(2014, 12, 28)), equalTo(false));
+
+        assertThat(isABankHoliday(LocalDate.of(2015, 12, 25)), equalTo(true));
+        assertThat(isABankHoliday(LocalDate.of(2015, 12, 26)), equalTo(false));
+        assertThat(isABankHoliday(LocalDate.of(2015, 12, 27)), equalTo(false));
+        assertThat(isABankHoliday(LocalDate.of(2015, 12, 28)), equalTo(true));
+
+        assertThat(isABankHoliday(LocalDate.of(2016, 12, 25)), equalTo(false));
+        assertThat(isABankHoliday(LocalDate.of(2016, 12, 26)), equalTo(true));
+        assertThat(isABankHoliday(LocalDate.of(2016, 12, 27)), equalTo(true));
+        assertThat(isABankHoliday(LocalDate.of(2016, 12, 28)), equalTo(false));
+
+        assertThat(isABankHoliday(LocalDate.of(2021, 12, 25)), equalTo(false));
+        assertThat(isABankHoliday(LocalDate.of(2021, 12, 26)), equalTo(false));
+        assertThat(isABankHoliday(LocalDate.of(2021, 12, 27)), equalTo(true));
+        assertThat(isABankHoliday(LocalDate.of(2021, 12, 28)), equalTo(true));
     }
 }
